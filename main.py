e0b403000f46216ed6955cb23fc9a114f858558e
EricPesci <89841427+ericpesci@users.noreply.github.com>
2025-06-11 19:10:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 @app.post("/")
 async def root(request: Request):
     data = await request.json()
-    print("MENSAJE RECIBIDO:", data)   # Este print lo ves en los logs de Render
+    print("MENSAJE RECIBIDO:", data)
     return {"msg": "PG-One responde", "data": data}
